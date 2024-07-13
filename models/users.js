510d41f7d9e3347f93f9e0af87d525a83d6f3710
Charlie <charlieboyle252@gmail.com>
2024-07-13 17:35:09 -0500

    initial set up of folders beginning of users.js code

---
+++ models/users.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reactions');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    first: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    last: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    github: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('student', usersSchema);
+
+module.exports = User;
